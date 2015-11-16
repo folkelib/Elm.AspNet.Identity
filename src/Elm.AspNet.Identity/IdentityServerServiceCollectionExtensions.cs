b02ede99e795b9d012943860f5e68688d29fbfdd
Sidoine De Wispelaere <sidoine@sidoine.net>
2015-11-16 21:55:49 +0100

    Add ServiceCollection extensions

---
+++ src/Elm.AspNet.Identity/IdentityServerServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Elm.AspNet.Identity;
+using Microsoft.AspNet.Identity;
+
+namespace Microsoft.Framework.DependencyInjection
+{
+    public static class ElmAspNetIdentityServiceCollectionExtensions
+    {
+        public static IServiceCollection AddElmIdentity<TUser, TKey>(this IServiceCollection services)
+            where TUser : IdentityUser<TUser, TKey>, new()
+            where TKey : IEquatable<TKey>
+        {
+            services.AddScoped<IUserStore<TUser>, UserStore<TUser, TKey>>();
+            services.AddScoped<IRoleStore<IdentityRole<TKey>>, RoleStore<IdentityRole<TKey>, TKey>>();
+            return services;
+        }
+    }
+}
